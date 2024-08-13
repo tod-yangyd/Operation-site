114c52cfbd59272a25014737facd56e127b25846
yangyedong <tod.yangyd@foxmail.com>
2024-08-13 16:39:52 +0800

    优化一下描述

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(dingtalkConfig):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = '119161229@qq.com'
+    FLASKY_MAIL_SENDER = 'xxxx@qq.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
