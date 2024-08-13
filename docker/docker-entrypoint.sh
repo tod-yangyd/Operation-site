aae855098acf8bf30ae9145b5a347c6891c711d3
yangyedong <tod.yangyd@foxmail.com>
2024-08-13 17:18:06 +0800

    添加dockerfile相关配置

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-service mysql start
 
-cd ui
-npm run dev&
-cd ..
+
+
 
 #python3 manager.py runserver --host 0.0.0.0
-python3 -m flask --app start run -h 0.0.0.0
+uwsgi --ini start.ini
