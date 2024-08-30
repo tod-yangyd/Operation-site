9ec1ab9c60dbb551037e35a145be8f0a1295223c
yangyedong <tod.yangyd@foxmail.com>
2024-08-30 12:34:20 +0800

    docker配置调整

--- docker/docker-buildx.sh
+++ docker/docker-buildx.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-app_name=${1:-'Operation-site'}
+app_name=${1:-'operation-site'}
 version=${2:-`date +%Y-%m%d-%H%m`}
 harbor_url=${3:-'172.17.200.84:80'}
-git_branch=${4:-'main'}
+harbor_project=${4:-'yyd'}
+git_branch=${5:-'main'}
 
 #release=$(git describe --abbrev=0 --tags)
 
 docker buildx build --push --no-cache \
 	--build-arg APP_Version="${version}" \
     --build-arg APP_Name="${app_name}" \
-	-t "${harbor_url}/yyd/${app_name}-${git_branch}:${version}" \
+	-t "${harbor_url}/${harbor_project}/${app_name}-${git_branch}:${version}" \
 	-f Dockerfile .
 
 docker buildx prune -f
