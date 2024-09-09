4c8f34fc8dce49a207240d5a4b4f40f762e0234d
yangyedong <tod.yangyd@foxmail.com>
2024-09-09 15:07:54 +0800

    告警接口逻辑优化

--- app/routes/api.py
+++ app/routes/api.py
@@ -6,47 +6,20 @@ import requests
 from ..models import dingtalk
 from ..base import base
 
+debug = False
+
 @base.route('/api/healthcheck')
 def healthcheck():
     return jsonify({'results': 'success'})
 
-
-@base.route('/api/alert', methods=['POST'])
-def alert():
-    headers = {'Content-Type': 'application/json'}
-    webhook = dingtalk.get_webhook(1,'Production')  # 主要模式有 0 ： 敏感字 1：# 敏感字 +加签 3：敏感字+加签+IP
-    data = request.get_json()
-    for i in data:
-        msg = {
-            "scope": i['scope'],
-            "name": i['name'],
-            "rule_name": i['ruleName'],
-            "alarm_message": i['alarmMessage'],
-            "start_time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(i['startTime']) / 1000)),
-            "tags": str(i['tags'])
-        }
-        send_msg_tpl = {
-            "msgtype": "text",
-            "text": {
-                "content": "服务名称：{scope} \n影响范围：{name} \n触发规则：{rule_name} \n开始时间：{start_time} \n告警内容：{alarm_message} "
-                           "\n告警标签：{tags}".format(**msg)
-            },
-            "at": {
-                "atUserIds": [
-                    "manager5345"
-                ],
-                "isAtAll": False
-            }
-        }
-        requests.post(webhook, data=json.dumps(send_msg_tpl), headers=headers)
-        time.sleep(0.2)
-    return jsonify({'results': 'success'})
 
 @base.route('/api/alert/<string:env>', methods=['POST'])
 def alert_env(env):
     headers = {'Content-Type': 'application/json'}
     webhook = dingtalk.get_webhook(1, env)  # 主要模式有 0 ： 敏感字 1：# 敏感字 +加签 3：敏感字+加签+IP
     data = request.get_json()
+    if debug:
+        print(data)
     for i in data:
         msg = {
             "scope": i['scope'],
@@ -71,6 +44,35 @@ def alert_env(env):
         }
         requests.post(webhook, data=json.dumps(send_msg_tpl), headers=headers)
         time.sleep(0.2)
+    return jsonify({'results': 'success'})
+
+
+@base.route('/api/log_alert/<string:env>', methods=['POST'])
+def log_alert(env):
+    headers = {'Content-Type': 'application/json'}
+    webhook = dingtalk.get_webhook(1,env)  # 主要模式有 0 ： 敏感字 1：# 敏感字 +加签 3：敏感字+加签+IP
+    data = request.get_json()
+
+    msg = {
+        "name": data['name'],
+        "hostip": data['hostip'],
+        "msg": data['msg'],
+        "appname": data['appname'],
+        "start_time": data['timestamp']
+    }
+    send_msg_tpl = {
+        "msgtype": "text",
+        "text": {
+            "content": "告警名称：{name} \n应用名称：{appname} \n告警地址：{hostip} \n开始时间：{start_time} \n告警内容：{msg} ".format(**msg)
+        },
+        "at": {
+            "atUserIds": [
+                "manager5345"
+            ],
+            "isAtAll": False
+        }
+    }
+    requests.post(webhook, data=json.dumps(send_msg_tpl), headers=headers)
     return jsonify({'results': 'success'})
 
 
